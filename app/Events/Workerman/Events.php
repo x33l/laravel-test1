b2d3426073646e4f4570e96e717fcb706d75d948
cc <1174311325@qq.com>
2020-05-29 08:08:04 +0000

    测试workerman

---
+++ app/Events/Workerman/Events.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Events\Workerman;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class Events
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public static function onWorkerStart($businessWorker)
+    {
+    }
+
+    public static function onConnect($client_id)
+    {
+    }
+
+    public static function onWebSocketConnect($client_id, $data)
+    {
+    }
+
+    public static function onMessage($client_id, $message)
+    {
+    }
+
+    public static function onClose($client_id)
+    {
+    }
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new PrivateChannel('channel-name');
+    }
+}
